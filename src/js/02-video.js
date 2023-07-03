78fb6b45089d080cb50e26edb98a8be4963da1aa
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-07-03 21:18:40 +0300

    ДЗ 8 виправ.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,25 +3,11 @@ import throttle from 'lodash.throttle';
 const videoPlayer = document.querySelector('iframe');
 const iframePlayer = new Player(videoPlayer);
 
-const onPlay = function(data) {
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay (data) {
+    console.log(data.seconds);
     localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-iframePlayer.on('timeupdate', throttle(onPlay, 1000));
-
-const currentTime = localStorage.getItem('videoplayer-current-time');
-
-
-iframePlayer.setCurrentTime(currentTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+iframePlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
