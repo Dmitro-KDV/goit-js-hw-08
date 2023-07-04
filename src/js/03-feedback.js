25b823841cee6ccdc171704298f6bb1f7beae269
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-07-04 14:45:32 +0300

    ДЗ 8 виправ.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,26 +4,22 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector(".feedback-form");
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
-
-updateOutput();
-form.addEventListener('input', throttle(saveMessage, 500));
-form.addEventListener("submit", formSubmit);
-
 const savedSettings = localStorage.getItem(LOCALSTORAGE_KEY);
 const parsedSettings = JSON.parse(savedSettings);
 
+form.addEventListener('input', throttle(saveMessage, 500));
+form.addEventListener("submit", formSubmit);
+
+updateOutput();
 function saveMessage(evt) {
   const formElements = {
         email: form.elements.email.value,
         message: form.elements.message.value,
       }
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formElements));
-  updateOutput();
 }
 
 function updateOutput() {
-  // const savedSettings = localStorage.getItem(LOCALSTORAGE_KEY);
-  // const parsedSettings = JSON.parse(savedSettings);
   if (parsedSettings) {
     form.elements.email.value = parsedSettings.email || "";
     form.elements.message.value = parsedSettings.message || "";
