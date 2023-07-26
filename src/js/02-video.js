4faf0f493ac51d0c3d0b91c8472e308fbb5978b9
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-07-26 12:35:45 +0300

    ДЗ 8 виправ 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,4 +9,24 @@ function onPlay (data) {
     localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-iframePlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+// iframePlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+
+
+iframePlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+    
+    iframePlayer.setColor('#45a247').then(function (color) {
+        // the color that was set
+    }).catch(function (error) {
+        // an error occurred setting the color
+    });
